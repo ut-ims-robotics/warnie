eee08820f5976a66ea0251393b2e485eafbdded9
RValner <valnerrobert@gmail.com>
2018-10-31 17:54:30 +0200

    Track layout is categorized

--- warnie/scripts/data_processing_scripts/reformat_data.py
+++ warnie/scripts/data_processing_scripts/reformat_data.py
@@ -7,10 +7,11 @@ import math
 # Class for maintaining visual cue box data
 #
 class VisualCueBox:
-    def __init__(self, cue_type, x, y):
+    def __init__(self, cue_type, x, y, z):
         self.cue_type = cue_type
         self.x = x
         self.y = y
+        self.z = z
 
     def toJsonStr(self):
         # Return Json string
@@ -47,20 +48,35 @@ def precategorizeTrapData(track_layout_file):
     track_layout_csv = open(track_layout_file)
     track_layout_reader = csv.DictReader(track_layout_csv)
 
-    for track_element in track_layout_reader:
-        name = track_element['object_name']
+    for idx, track_element in enumerate(track_layout_reader):
+        full_name = track_element['object_name']
+        trap_name = full_name.split('::')[1]
+
+        if ("trap" in trap_name) or ("vis_cue" in trap_name):
+            if trap_name in precat_trap_data:
+                precat_trap_data[trap_name].append(full_name)
+            else:
+                precat_trap_data[trap_name] = []
+                precat_trap_data[trap_name].append(full_name)
 
     return precat_trap_data
 
-# in_file_trajectory_file = str(sys.argv[1])
-# in_file_track_layout = str(sys.argv[2])
+in_file_trajectory_file = str(sys.argv[1])
+in_file_track_layout = str(sys.argv[2])
 # out_file_yaml = str(sys.argv[3])
+
+precat_trap_data = precategorizeTrapData(in_file_track_layout)
+
+for key, value in precat_trap_data.iteritems():
+    print key
+
+    for val in value:
+        print "   " + val
 
 # with open('data.json', 'w') as outfile:
 #     json.dump(data, outfile)
 
-data_dictionary = {}
-data_json = json.dumps(data_dictionary)
+# data_dictionary = {}
+# data_json = json.dumps(data_dictionary)
+# print data_json
 
-
-#print data_json
