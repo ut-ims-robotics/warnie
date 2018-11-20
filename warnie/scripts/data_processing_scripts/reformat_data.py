383b2b02b5ce1d7216e568ae9b3d0de8f6ff753d
RValner <valnerrobert@gmail.com>
2018-11-20 18:07:07 +0200

    sookie format 1

--- warnie/scripts/data_processing_scripts/reformat_data.py
+++ warnie/scripts/data_processing_scripts/reformat_data.py
@@ -3,36 +3,10 @@ import sys
 import os
 import matplotlib.pyplot as plt
 import numpy as np
+from copy import copy
 import warnie_trap as wt
 import warnie_plot as wp
 import warnie_trajectory as wtr
-
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-#
-#   "MAIN"
-#
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-
-# Get the paths to trajectory data, track layout data and JSON output data
-'''
-in_file_trajectory_file = str(sys.argv[1])
-in_file_track_layout = str(sys.argv[2])
-out_file_json = str(sys.argv[3])
-'''
-
-'''
-# Extract the trap classes
-precat_trap_data = precategorizeTrapData(in_file_track_layout)
-trap_classes = rawTrapToClass(precat_trap_data)
-'''
-
-# Write the trap data to a JSON file
-# trap_classes_json = json.dumps(trap_classes, cls=ComplexEncoder, sort_keys=True, indent=2)
-# with open(out_file_json, 'w') as out_file:
-#     out_file.write(trap_classes_json)
-
-# Open the trajectory file and convert it into numpy array
-
 
 # Get the data directories
 in_base_path = str(sys.argv[1])
@@ -40,7 +14,6 @@ data_paths_raw = os.listdir(in_base_path)
 data_paths = []
 
 # Prepare the plotting things
-# fig, plot = plt.subplots()
 plt.axis('equal')
 plt.axis([-10, 10, -3, 3])
 images_dictionary = wp.createImagesDictionary(in_base_path + "/images/")
@@ -71,6 +44,10 @@ for data_path in data_paths:
         trajectory_segment = wtr.getTrajectorySegment(trajectory_data_np, 20, trap_class.mean_pos_x)
         trap_inst_tuple = (trap_class, trajectory_segment)
 
+        # Dont append bad data
+        if len(trajectory_segment) <= 0:
+            continue
+
         # Insert the tuple into the map, but fist check if the key exists or not
         if trap_class.name in trap_data_dict:
             trap_data_dict[trap_class.name].append(trap_inst_tuple)
@@ -78,27 +55,94 @@ for data_path in data_paths:
             trap_data_dict[trap_class.name] = []
             trap_data_dict[trap_class.name].append(trap_inst_tuple)
 
-# Create trajectory images, one per each trap type
-print "Creating trajectory images"
+# Balance the trajectory data
+trap_data_balanced_dict = {}
+min_elements_global = 9999999
+
 for trap_type, trap_tuples in trap_data_dict.iteritems():
-
-    print "Working on trap '" + trap_type + "'"
-    plt.axis('equal')
-    plt.axis([-10, 10, -3, 3])
-    wp.addTrackEdges(plt, 20, 5.45)
-    wp.addBoxImages(trap_tuples[0][0], plt, images_dictionary, 0.35)
-
-    # Iterate through all trap data segments per current trap type
     for trap_tuple in trap_tuples:
         trap_class = trap_tuple[0]
         trajectory_segment = trap_tuple[1]
+        min_elements = wtr.findMinDataPoints(trajectory_segment[:, 0], trap_class.mean_pos_x)
 
-        plt.plot(trajectory_segment[:, 0] - trap_class.mean_pos_x, trajectory_segment[:, 1], "k-", alpha=0.2)
+        if (min_elements < min_elements_global) and (min_elements > 0):
+            min_elements_global = min_elements
 
-    # Save the figure
-    plt.savefig(in_base_path + "/data_visual/" + trap_type, dpi=500)
+print "global min elements: ", min_elements_global
+losses = []
 
-    # Clear the plot
-    plt.clf()
+for trap_type, trap_tuples in trap_data_dict.iteritems():
+    for trap_tuple in trap_tuples:
+        trap_class = trap_tuple[0]
+        trajectory_segment = trap_tuple[1]
+        idx = wtr.findXElementIndex(trajectory_segment[:, 0], trap_class.mean_pos_x)
 
-print "Finished. All images saved to " + in_base_path + "/data_visual/"
+        balanced_trajectory_segment = wtr.balanceData(trajectory_segment, idx, min_elements_global)
+        loss = 100*(len(trajectory_segment) - len(balanced_trajectory_segment)) / len(trajectory_segment)
+        losses.append(loss)
+
+        # print ("before: ", len(trajectory_segment), ", after:  ", len(balanced_trajectory_segment), ", loss: ", loss)
+
+        trap_inst_tuple = (trap_class, balanced_trajectory_segment)
+
+        # Insert the tuple into the map, but fist check if the key exists or not
+        if trap_class.name in trap_data_balanced_dict:
+            trap_data_balanced_dict[trap_class.name].append(trap_inst_tuple)
+        else:
+            trap_data_balanced_dict[trap_class.name] = []
+            trap_data_balanced_dict[trap_class.name].append(trap_inst_tuple)
+
+print "Mean loss is:", np.mean(losses), ", with std_dev:", np.std(losses)
+
+# Save the data
+single_header = ["x", "y", "z", "yaw", "t"]
+# np.set_printoptions(precision=2)
+
+for trap_type, trap_tuples in trap_data_balanced_dict.iteritems():
+    # prepare the csv header
+    nr_of_subjs = len(trap_tuples)
+    header = []
+    csv_data_np = copy(trap_tuples[0][1])
+    csv_data_np[:, 0] = csv_data_np[:, 0] - trap_tuples[0][0].mean_pos_x
+
+    for i in range(0, nr_of_subjs):
+        header += single_header
+
+    for i in range(1, len(trap_tuples)):
+        csv_data_np_add = trap_tuples[i][1]
+        csv_data_np_add[:, 0] = csv_data_np_add[:, 0] - trap_tuples[i][0].mean_pos_x
+        csv_data_np = np.concatenate((csv_data_np, csv_data_np_add), axis=1)
+
+    csv_data_np = np.round(csv_data_np, decimals=3, out=None)
+
+    output_file = in_base_path + "/data_cropped/" + trap_type + ".csv"
+
+    with open(output_file, 'w') as csvfile:
+        writer = csv.writer(csvfile, delimiter=",")
+        writer.writerow(header)
+        writer.writerows(csv_data_np)
+
+# Create trajectory images, one per each trap type
+# print "Creating trajectory images"
+# for trap_type, trap_tuples in trap_data_balanced_dict.iteritems():
+#
+#     print "Working on trap '" + trap_type + "'"
+#     plt.axis('equal')
+#     plt.axis([-10, 10, -3, 3])
+#     wp.addTrackEdges(plt, 20, 5.45)
+#     wp.addBoxImages(trap_tuples[0][0], plt, images_dictionary, 0.35)
+#
+#     # Iterate through all trap data segments per current trap type
+#     for trap_tuple in trap_tuples:
+#         trap_class = trap_tuple[0]
+#         trajectory_segment = trap_tuple[1]
+#
+#         plt.plot(trajectory_segment[:, 0] - trap_class.mean_pos_x, trajectory_segment[:, 1], "k-", alpha=0.2)
+#
+#     # Save the figure
+#     plt.savefig(in_base_path + "/data_visual/data_vis_trim/" + trap_type, dpi=500)
+#
+#     # Clear the plot
+#     plt.clf()
+#
+# print "Finished. All images saved to " + in_base_path + "/data_visual/data_vis_trim/"
--- warnie/scripts/data_processing_scripts/warnie_trajectory.py
+++ warnie/scripts/data_processing_scripts/warnie_trajectory.py
@@ -1,7 +1,10 @@
+import random
+import numpy as np
+
 #
 # Segments the data based on the center point and the range around the center
 #
-def getTrajectorySegment(data, range, center_point):
+def getTrajectorySegment(trajectory_data, range, center_point):
     start_idx = 0
     end_idx = 0
 
@@ -10,7 +13,7 @@ def getTrajectorySegment(data, range, center_point):
 
     start_idx_found = False
 
-    for idx, trajectory_point in enumerate(data):
+    for idx, trajectory_point in enumerate(trajectory_data):
         if start_idx_found != True and trajectory_point[0] >= start_dist:
             start_idx = idx
             start_idx_found = True
@@ -19,4 +22,67 @@ def getTrajectorySegment(data, range, center_point):
             end_idx = idx
             break
 
-    return data[start_idx:end_idx + 1]
+    return trajectory_data[start_idx:end_idx + 1]
+
+#
+# Finds the index of the element, which has its "x" closest to given value
+#
+def findXElementIndex(trajectory_data, x_val):
+    min_diff_idx = 0
+    min_x_val_diff = 9999999
+
+    for idx, trajectory_point in enumerate(trajectory_data):
+        x_val_diff = abs(trajectory_point - x_val)
+
+        if x_val_diff < min_x_val_diff:
+            min_diff_idx = idx
+            min_x_val_diff = x_val_diff
+
+    return min_diff_idx
+
+#
+# Finds the min nr of datapoints from given value
+#
+def findMinDataPoints(trajectory_data, x_val):
+    idx = findXElementIndex(trajectory_data, x_val)
+    nr_of_elems_left = idx
+    nr_of_elems_right = len(trajectory_data) - nr_of_elems_left - 1
+
+    # minim = min(nr_of_elems_left, nr_of_elems_right)
+    # print "right: ", nr_of_elems_right, ", left: ", nr_of_elems_left, ", min: ", minim, ", total: ", len(trajectory_data)
+    return min(nr_of_elems_left, nr_of_elems_right)
+
+#
+# Removes the given amount of points in the data
+#
+def removeDatapoints(trajectory_data, nr_of_points):
+    idxs = random.sample(range(len(trajectory_data)), nr_of_points)
+    new_array = np.delete(trajectory_data, idxs, axis=0)
+    # print "removed", len(trajectory_data) - len(new_array), "datapoints"
+    #return np.delete(trajectory_data, idxs)
+    return new_array
+
+#
+# Balances the amount of datapoints to given amount around given index
+#
+def balanceData(trajectory_data, center_idx, balance_count):
+    data_left = trajectory_data[0: center_idx]
+    data_right = trajectory_data[center_idx + 1:]
+    data_center = trajectory_data[center_idx]
+
+    # print "left: ", len(data_left), ", right: ", len(data_right), ", total: ", len(trajectory_data), ", center: ", data_center[0], " at_idx: ", center_idx
+
+    balance_diff_left = len(data_left) - balance_count
+    balance_diff_right = len(data_right) - balance_count
+
+    # print "diff_left: ", balance_diff_left, ", diff_right: ", balance_diff_right
+
+    if balance_diff_left > 0:
+        data_left = removeDatapoints(data_left, balance_diff_left)
+
+    if balance_diff_right > 0:
+        data_right = removeDatapoints(data_right, balance_diff_right)
+
+    balanced_array = np.concatenate((data_left, [data_center]), axis=0)
+    balanced_array = np.concatenate((balanced_array, data_right), axis=0)
+    return balanced_array
