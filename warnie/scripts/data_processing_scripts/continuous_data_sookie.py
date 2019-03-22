86ccec28226bd9d6c89f0a1208ffc6e6c38ae3d1
RValner <valnerrobert@gmail.com>
2019-03-22 17:03:53 +0200

    y intercept data for exp3

--- warnie/scripts/data_processing_scripts/continuous_data_sookie.py
+++ warnie/scripts/data_processing_scripts/continuous_data_sookie.py
@@ -37,6 +37,21 @@ def getCrossingIndex(trajectory_data):
 
     return crossing_idx
 
+#
+# Reformats the data
+# 
+def reformatOutput(interception_data):
+    data_dict = dict()
+
+    for i_d in interception_data:
+        if i_d[1] in data_dict:
+            data_dict[i_d[1]].append(i_d[0])
+        else:
+            data_dict[i_d[1]] = []
+            data_dict[i_d[1]].append(i_d[0])
+
+    return data_dict
+
 # ------------------------------------  End of function definitions  ------------------------------------
 
 # Get the path that was specified from the command line
@@ -75,22 +90,57 @@ for trap_name in trap_names:
 
         y_intercept = abs(y2 - (y2-y1)/(x2-x1)*x2)
         y_intercept = round(y_intercept, 4)
-        interception_tuple = [y_intercept, trap_name]
+
+        trap_type = ""
+        if ("_n_" in trap_name):
+            trap_type = "neutral"
+        elif ("_a_" in trap_name):
+            trap_type = "cueK"
+        elif ("_r_" in trap_name):
+            trap_type = "cueA"  
+
+        trap_group = ""
+        if (trap_name.endswith("_4")):
+            trap_group = "c11"
+        elif (trap_name.endswith("_3")):
+            trap_group = "c12"
+        elif (trap_name.endswith("_1")):
+            trap_group = "c21"
+        elif (trap_name.endswith("_2")):
+            trap_group = "c22"  
+
+        interception_tuple = [y_intercept, trap_name, trap_type, trap_group]
         interception_data.append(interception_tuple)
 
 # Print the data out
 for d in interception_data:
-    print d[1], d[0]
+    print d[1], d[0], d[2], d[3]
+
+# Convert to dictionary format
+#data_dict = reformatOutput(interception_data)
+
+#for key, value in data_dict.iteritems():
+#    print key
+
+#    print len(value)
+
+#    print "--------"
+
+#output_file_csv = in_data_base_path + "/continuous_5block_1.csv"
+#with open(output_file_csv, 'wb') as f:  # Just use 'w' mode in 3.x
+#    w = csv.writer(f, delimiter = "\t")
+#    w.writerow(data_dict.keys())
+#    w.writerows(zip(*[data_dict[key] for key in data_dict.keys()]))
 
 #
 # UNCOMMENT THE FOLLOWING CODE IF YOU WANT TO STORE THE DATA
 #
 
-# # Save the data to a CSV file
-# output_file_csv = in_data_base_path + "/test.csv"
-# with open(output_file_csv, 'w') as csvfile:
-#     writer = csv.writer(csvfile, delimiter=",")
-#     writer.writerows(interception_data)
+# Save the data to a CSV file
+output_file_csv = in_data_base_path + "/continuous_5block.csv"
+with open(output_file_csv, 'w') as csvfile:
+   writer = csv.writer(csvfile, delimiter=",")
+   writer.writerows(interception_data)
 
 
 
